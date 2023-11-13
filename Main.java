ee8da04cda2031c1357ce78520904491f521bc3a
hyper <hyperskill@hyperskill.org>
2023-11-13 21:12:54 +0600

    Stage 3/6 v2

--- Main.java
+++ Main.java
@@ -4,62 +4,6 @@ import java.util.*;
 
 public class Main {
 
-    private static void mergeSort(Integer[] inputArray) {
-        int inputLength = inputArray.length;
-
-        if (inputLength < 2) {
-            return;
-        }
-
-        int midIndex = inputLength / 2;
-        Integer[] leftHalf = new Integer[midIndex];
-        Integer[] rightHalf = new Integer[inputLength - midIndex];
-
-        for (int i = 0; i < midIndex; i++) {
-            leftHalf[i] = inputArray[i];
-        }
-        for (int i = midIndex; i < inputLength; i++) {
-            rightHalf[i - midIndex] = inputArray[i];
-        }
-
-        mergeSort(leftHalf);
-        mergeSort(rightHalf);
-
-        merge(inputArray, leftHalf, rightHalf);
-
-    }
-    private static void merge (Integer[] inputArray, Integer[] leftHalf, Integer[] rightHalf) {
-        int leftSize = leftHalf.length;
-        int rightSize = rightHalf.length;
-
-        int i = 0, j = 0, k = 0;
-
-        while (i < leftSize && j < rightSize) {
-            if (leftHalf[i] <= rightHalf[j]) {
-                inputArray[k] = leftHalf[i];
-                i++;
-            }
-            else {
-                inputArray[k] = rightHalf[j];
-                j++;
-            }
-            k++;
-        }
-
-        while (i < leftSize) {
-            inputArray[k] = leftHalf[i];
-            i++;
-            k++;
-        }
-
-        while (j < rightSize) {
-            inputArray[k] = rightHalf[j];
-            j++;
-            k++;
-        }
-
-    }
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
@@ -70,15 +14,13 @@ public class Main {
 
                 integerList.add(scanner.nextInt());
             }
-            Integer[] integerArray = new Integer[integerList.size()];
-            integerArray = integerList.toArray(integerArray);
-            mergeSort(integerArray);
+            Collections.sort(integerList);
 
             System.out.printf("Total numbers: %d%n" +
-                            "Sorted data: %s", integerList.size(), Arrays.toString(integerArray)
-                            .replace(",", "")
+                            "Sorted data: %s", integerList.size(), integerList.toString()
                     .replace("[", "")
-                    .replace("]", ""));
+                    .replace("]", "")
+                    .replace(",", " "));
 
         } else {
 
